c214f07ae9ee87f7f8112331a2adee837a1168c4
debasistripathy01 <debasistripathy04@gmail.com>
2022-12-13 22:41:18 +0530

    store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,1 +1,15 @@
-console.log("dummy")
+import { cartProdCount, getCartProd } from "./actionType";
+
+export const fetchCartData = (data) => {
+   return {
+    type: getCartProd,
+    payload: data,
+  };
+};
+
+export const cartProductCount = (data) => {
+   return {
+    type: cartProdCount,
+    payload: data,
+  };
+};

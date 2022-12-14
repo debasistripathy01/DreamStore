8100068ccea7b61a10ad71bd4bb4acf7468e6e4b
Debasis Tripathy <104076012+debasistripathy01@users.noreply.github.com>
2022-12-14 23:50:31 +0530

    changes (#15) (#16)
    * changes
    
    * change
    
    * abcd
    
    Co-authored-by: Devender Yadav <103569870+Devender0014@users.noreply.github.com>

--- src/Components/LoginAuth/Signup.jsx
+++ src/Components/LoginAuth/Signup.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
-import "./signup.css";
+import "./signup.css"
+import {
+    Heading,
+    Text,
+    Button, Box, Image
+} from '@chakra-ui/react'
 
 
-export const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+
+function Signup() {
+
+
+    return (
+        <div className="mainSignup">
+            <div className="SignupNavBar">
+                <img src="https://s1.thcdn.com/enterprise/assets/dermstore-global-a3ce7f42-4a2c-4cc5-ad05-ba594900fa02-logo-default.svg" alt="" />
+            </div>
+            <div>
+                <div className="form">
+                    <Heading className="head">About You</Heading>
+                    <Text>Sign Up With</Text>
+                    <Box display={"flex"} gap="40px" marginBottom={"60px"}>
+                        <Button className="button">
+                            <Image boxSize='25px' src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/2048px-Facebook_icon.svg.png" />
+                            <Text>Facebook</Text>
+                        </Button>
+                        <Button className="button">
+                            <Image boxSize='25px' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png" />
+                            <Text>Google</Text>
+                        </Button>
+                    </Box>
+                    <hr />
+                    
+                    <Text>Or create an email account</Text>
+                    <div >
+                        <form>
+                            <div>
+                                <label>* Full Name</label>
+                                <input type='text' />
+                            </div>
+                            <div>
+                                <label>* Email address</label>
+                                <input type='email' />
+                            </div>
+                            <div>
+                                <label>* Confirm Email address</label>
+                                <input type='email' />
+                            </div>
+                            <div>
+                                <label>* Password</label>
+                                <input type='password' />
+                            </div>
+                            <div>
+                                <label>* Confirm Password</label>
+                                <input type='password' />
+                            </div>
+                            <div>
+                                <label>Cell Phone Number (Optional)</label>
+                                <input type='number' />
+                                <p>We will use this number to send occasional promotional messages.</p>
+                            </div>
+                            <div className="referral">
+                                <label>Referral Code (Optional)</label>
+                                <input type='text' />
+                                <p>* Your referrals discount is automatically applied at cart</p>
+                            </div>
+                                <h3>Loyalty Reward program</h3>
+                            <div className="checkBox">
+                                <input type="checkbox"/>
+                                <label>Include me in the Dermstore Rewards program Read our terms of use.</label>
+                            </div>
+                            <div>
+                                <button>CONTINUE</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default Signup
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,4 @@
-import {
-  applyMiddleware,
-  combineReducers,
-  compose,
-  legacy_createStore,
-} from "redux";
+
 import thunk from "redux-thunk";
 import { CartReducer } from "./cart/ReducerCart";
 import { reducer as productReducer } from "./ProdData/reducer";
@@ -12,7 +7,3 @@ const rootReducer = combineReducers({ productReducer, CartReducer });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = legacy_createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);

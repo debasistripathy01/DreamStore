803a9a93de37793778d75593cac62bef9890d6ba
HARSHAL-AP <apsundeharshal129@gmail.com>
2022-12-14 23:07:22 +0530

    navbar outline and admin page structure

---
+++ src/Components/Admin/Addproduct.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Addproduct = () => {
+  return (
+    <div>Addproduct</div>
+  )
+}
---
+++ src/Components/Admin/Orders.jsx
@@ -0,0 +1,8 @@
+ import React from 'react'
+ 
+ export const Orders = () => {
+   return (
+     <div>Orders</div>
+   )
+ }
+ 

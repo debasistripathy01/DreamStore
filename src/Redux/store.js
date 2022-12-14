90dd08df10c214cfeb327001c8d036ec535eb671
shagun <shagun08081999@gmail.com>
2022-12-14 22:30:41 +0530

    store done

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,17 @@
 
-import { legacy_createStore as createStore } from "redux";
-import { reducer } from "./cart/ReducerCart";
+import { applyMiddleware, combineReducers, compose, legacy_createStore} from "redux";
+import thunk from "redux-thunk";
+ 
+import {reducer as productReducer} from "./ProdData/reducer"
 
-export const store = createStore(reducer);
+const rootReducer = combineReducers({productReducer})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = legacy_createStore(rootReducer, 
+      composeEnhancers(applyMiddleware(thunk))
+    );
+
 
 
 

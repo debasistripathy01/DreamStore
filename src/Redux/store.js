0f83aed3241725c99859581a9f21255bafc7f8e4
Debasis Tripathy <104076012+debasistripathy01@users.noreply.github.com>
2022-12-14 23:58:05 +0530

    Fw18 0821 day 1 (#18)
    * day-1-homepage-update
    
    * update

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,8 @@
 
+
+import { applyMiddleware , combineReducers, compose, legacy_createStore} from "redux";
+
+
 import thunk from "redux-thunk";
 import { CartReducer } from "./cart/ReducerCart";
 import { reducer as productReducer } from "./ProdData/reducer";
